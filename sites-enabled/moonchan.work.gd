4e2fa44a632f7ab9b7d5f5f6e730067ea169cf50
Hana-ame <luminovoez@gmail.com>
2025-11-26 10:17:42 +0000

    moonchan.work.gd

--- sites-enabled/moonchan.work.gd
+++ sites-enabled/moonchan.work.gd
@@ -1,10 +1,10 @@
 server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
-    server_name  nft.run.place;
+    server_name moonchan.work.gd;
 
     # ssl configuration
-    include /etc/nginx/conf.d/ssl/nft.conf;
+    include /etc/nginx/conf.d/ssl/moonchan.work.gd.conf;
     # include /etc/nginx/conf.d/ssl/hana-sweet.top.conf;
     
     access_log /dev/null;
@@ -14,20 +14,19 @@ server {
 
     location / {
       add_header X-Frame-Options "ALLOWALL";
-      root /var/www/nft;
+      root /var/www/moonchan;
       try_files $uri $uri/ /index.html;
     }
 
     
-    # location / {
-    #   # root /var/www/acme-challenge;
-    #   proxy_set_header Host $http_host; # 必须
-    #   proxy_pass http://127.0.0.1:8080/;
-    # }
+    location /api/ {
+      proxy_set_header Host $http_host; # 必须
+      proxy_pass http://127.25.5.19:8080/api/;
+    }
 
     
-    # include /etc/nginx/conf.d/addon/doh.conf;
-    # include /etc/nginx/conf.d/addon/v2ray.conf;
+    include /etc/nginx/conf.d/addon/doh.conf;
+    include /etc/nginx/conf.d/addon/v2ray.conf;
 
     # redirect server error pages to the static page /50x.html
     error_page   500 502 503 504  /50x.html;
