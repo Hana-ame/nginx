2e9627968203819795881ffc2ef264511a592060
Hana-ame <luminovoez@gmail.com>
2025-11-27 11:15:17 +0000

    ssl

---
+++ sites-available/moonchan.work.gd
@@ -0,0 +1,74 @@
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name moonchan.work.gd;
+
+    # ssl configuration
+    include /etc/nginx/conf.d/ssl/moonchan.work.gd.conf;
+    # include /etc/nginx/conf.d/ssl/hana-sweet.top.conf;
+    
+    access_log /dev/null;
+    error_log /dev/null;
+
+    include /etc/nginx/conf.d/addon/general-deny.conf;
+
+    location / {
+      add_header X-Frame-Options "ALLOWALL";
+      root /var/www/moonchan;
+      try_files $uri $uri/ /index.html;
+    }
+
+    
+    location /api/ {
+      proxy_set_header Host $http_host; # 必须
+      proxy_pass http://127.25.5.19:8080/api/;
+    }
+
+    
+    include /etc/nginx/conf.d/addon/doh.conf;
+    include /etc/nginx/conf.d/addon/v2ray.conf;
+
+    # redirect server error pages to the static page /50x.html
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+    server_name  mstdn.work.gd;
+
+    # ssl configuration
+    include /etc/nginx/conf.d/ssl/meromeromeiro.top.conf;
+    # include /etc/nginx/conf.d/ssl/hana-sweet.top.conf;
+    
+    access_log /dev/null;
+    error_log /dev/null;
+
+    include /etc/nginx/conf.d/addon/general-deny.conf;
+
+    # location / {
+    #   add_header X-Frame-Options "ALLOWALL";
+    #   root /var/www/moonchan;
+    # }
+
+    
+    location / {
+      # root /var/www/acme-challenge;
+      proxy_set_header Host $http_host; # 必须
+      proxy_pass http://127.0.0.1:8080/;
+    }
+
+    
+    include /etc/nginx/conf.d/addon/doh.conf;
+    include /etc/nginx/conf.d/addon/v2ray.conf;
+
+    # redirect server error pages to the static page /50x.html
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+}
+
